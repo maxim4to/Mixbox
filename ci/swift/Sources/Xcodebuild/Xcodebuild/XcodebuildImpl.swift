282d081c99260cfca8a1598eaebddecad8a45892
Artyom Y. Razinov <arazinov@avito.ru>
2019-10-18 18:31:16 +0300

    Add disabled workaround for xcodebuild error and code to check if it works

--- ci/swift/Sources/Xcodebuild/Xcodebuild/XcodebuildImpl.swift
+++ ci/swift/Sources/Xcodebuild/Xcodebuild/XcodebuildImpl.swift
@@ -64,13 +64,10 @@ public final class XcodebuildImpl: Xcodebuild {
         
         let argsString = args.map { "\"\($0)\"" }.joined(separator: " ")
         
-        _ = try bashExecutor.executeOrThrow(
-            command:
-            """
-            set -o pipefail
-            xcodebuild \(argsString) | \(xcodebuildPipeFilter)
-            """,
-            currentDirectory: projectDirectory
+        try runXcodebuild(
+            projectDirectory: projectDirectory,
+            argsString: argsString,
+            xcodebuildPipeFilter: xcodebuildPipeFilter
         )
         
         // TODO: Try to remove? I think it is outdated.
@@ -85,6 +82,77 @@ public final class XcodebuildImpl: Xcodebuild {
         )
         
         return XcodebuildResult(derivedDataPath: derivedDataPath)
+    }
+    
+    private func runXcodebuild(
+        projectDirectory: String,
+        argsString: String,
+        xcodebuildPipeFilter: String)
+        throws
+    {
+        let bashResult = try bashExecutor.execute(
+            command:
+            """
+            set -o pipefail
+            xcodebuild \(argsString) | \(xcodebuildPipeFilter)
+            """,
+            currentDirectory: projectDirectory,
+            environment: .current,
+            stdoutDataHandler: { data in
+                FileHandle.standardOutput.write(data)
+            },
+            stderrDataHandler: { data in
+                FileHandle.standardError.write(data)
+            }
+        )
+        
+        let xcodebuildBugIndicatorString = "Failed to establish communication with the test runner"
+        
+        if bashResult.code != 0,
+            let stdout = bashResult.stdout.utf8String(),
+            stdout.contains(xcodebuildBugIndicatorString)
+        {
+            // This code should be uncommented if we knew what file descriptor is used in this error.
+            
+            // print("Found bug in Xcodebuild: \(xcodebuildBugIndicatorString), attempting to retry running xcodebuild.")
+            //
+            // _ = try bashExecutor.executeOrThrow(
+            //     command:
+            //     """
+            //     set -o pipefail
+            //     xcodebuild \(argsString) | \(xcodebuildPipeFilter)
+            //     """,
+            //     currentDirectory: projectDirectory
+            // )
+            
+            // Checking if it is in stdout or stderr:
+            print(
+                """
+                xcodebuildBugIndicatorString (\(xcodebuildBugIndicatorString)) is indeed \
+                inside stdout of xcodebuild, uncomment code in XcodebuildImpl and remove this \
+                code that was added to check if indicator is in stdout or stderr.
+                """
+            )
+            throw NonZeroExitCodeBashError(
+                bashResult: bashResult
+            )
+        } else {
+            // Checking if it is in stdout or stderr:
+            if let stderr = bashResult.stdout.utf8String(),
+                stderr.contains(xcodebuildBugIndicatorString)
+            {
+                print(
+                    """
+                    xcodebuildBugIndicatorString (\(xcodebuildBugIndicatorString)) is inside \
+                    stderr and not stdout! Correct the code accordingly.
+                    """
+                )
+            }
+            
+            throw NonZeroExitCodeBashError(
+                bashResult: bashResult
+            )
+        }
     }
     
     private func xcodebuildArgs(
